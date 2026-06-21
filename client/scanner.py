d25b945709549d935250eee308fa3fc10ce4145e
SanskarH56 <sanskarhande8@gmail.com>
2026-06-21 20:05:30 +0530

    feat add metadata extraction function

---
+++ client/scanner.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from dataclasses import dataclass 
+
+@dataclass
+class FileMetaData:
+    relPath: str
+    size: int
+    mtime: float
+    inode: int
+
+def extract_metadata(path: Path):
+    stat = path.stat()
+
+    metadata = FileMetaData(
+        relPath = path,
+        size = stat().st_size,
+        mtime = stat().st_mtime,
+        inode = stat().st_ino
+    )
+
+    return metadata
