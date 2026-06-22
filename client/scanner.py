fea7a03aad34b04b62418f2f5bf5c8cc9a6d4ea7
SanskarH56 <sanskarhande8@gmail.com>
2026-06-22 18:07:35 +0530

    feat add scan_dir function

--- client/scanner.py
+++ client/scanner.py
@@ -8,14 +8,14 @@ class FileMetaData:
     mtime: float
     inode: int
 
-def extract_metadata(path: Path):
+def extract_metadata(path: Path, root: Path):
     stat = path.stat()
 
     metadata = FileMetaData(
-        relPath = path,
-        size = stat().st_size,
-        mtime = stat().st_mtime,
-        inode = stat().st_ino
+        relPath = str(path.relative_to(root)),
+        size = stat.st_size,
+        mtime = stat.st_mtime,
+        inode = stat.st_ino
     )
 
     return metadata
@@ -29,6 +29,13 @@ def iter_files(root: Path):
 
     return files
 
+def scan_dir(root: Path):
+    Record = []
+    paths = iter_files(root)
 
+    for path in paths:
+        Record.append(extract_metadata(path,root))
+
+    return Record
 
 
