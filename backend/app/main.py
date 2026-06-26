33677c3245fec9238e238e89794439dbf30f584e
Meet S <meetshinde.official@gmail.com>
2026-06-26 02:53:35 +0000

    Used lifespan parameter (asynccontextmanager) instead of Deprecated on_startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.database import Base, engine
@@ -5,9 +7,17 @@ from app import models
 
 app = FastAPI(title="Filesync Backend")
 
-@app.on_event("startup")
+"""@app.on_event("startup")
 def on_startup():
+    Base.metadata.create_all(bind=engine)"""
+# on_event Deprecated, hence using Lifespan parameter
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Server startup...")
     Base.metadata.create_all(bind=engine)
+    yield
+    print("Server shutdown...")
 
 @app.get("/health")
 def health_check():
