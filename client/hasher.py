cb246ed19e38e6e8977824a820af48dbbbf69c78
SanskarH56 <sanskarhande8@gmail.com>
2026-06-28 16:39:06 +0530

    feat add hasher prototype version

---
+++ client/hasher.py
@@ -0,0 +1,20 @@
+import hashlib
+from scanner import FileMetaData
+
+def hash_file(path , fileinfo: FileMetaData):
+    hasher = hashlib.sha256()
+
+    for chunk in read_chunks(path):
+        hasher.update(chunk)
+
+    fileinfo.hash = hasher.hexdigest()
+
+    return fileinfo
+
+def read_chunks(path):
+    with open(path, "rb") as f:
+        while True:
+            chunk = f.read(1024 * 1024)
+            if not chunk:
+                break
+            yield chunk
