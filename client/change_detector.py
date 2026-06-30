61d29de0c5119ac55603e3e070580b6082616de4
SanskarH56 <sanskarhande8@gmail.com>
2026-06-30 21:35:08 +0530

    feat add detect added deleted and modified files function for later sync planner module

--- client/change_detector.py
+++ client/change_detector.py
@@ -21,6 +21,9 @@ class ChangeSet:
     modified: list[Change]
     unchanged: list[Change]
 
+def detect_changes(current_data : list[FileMetaData] , previous_data : list[FileMetaData]):
+    return None
+
 def build_lookup(data: list[FileMetaData]):
 
     lookup_table = {
@@ -29,4 +32,76 @@ def build_lookup(data: list[FileMetaData]):
     }
 
     return lookup_table
+
+def detect_added(curr_lookup : dict , old_lookup : dict):
+
+    added_changes = []
+
+    for path in curr_lookup:
+        if path not in old_lookup:
+            new_file = Change(
+                change_type = ChangeType.ADDED,
+                old_metadata = None,
+                new_metadata = curr_lookup[path]
+            )
+            
+            added_changes.append(new_file)
+
+    return added_changes
+
+def detect_deleted(curr_lookup : dict , old_lookup : dict):
+
+    deleted_changes = []
+
+    for path in old_lookup:
+        if path not in curr_lookup:
+            new_file = Change(
+                change_type = ChangeType.DELETED,
+                old_metadata = old_lookup[path],
+                new_metadata = None
+            )
+            
+            deleted_changes.append(new_file)
+
+    return deleted_changes
+
+def detect_modified(curr_lookup : dict , old_lookup : dict):
+
+    modified_changes = []
+
+    for path in curr_lookup:
+        if path in old_lookup:
+            if curr_lookup[path].size != old_lookup[path].size:
+                new_file = Change(
+                    change_type = ChangeType.MODIFIED,
+                    old_metadata = old_lookup[path],
+                    new_metadata = curr_lookup[path]
+                )
+
+                modified_changes.append(new_file)
+
+            elif curr_lookup[path].mtime != old_lookup[path].mtime:
+                new_file = Change(
+                    change_type = ChangeType.MODIFIED,
+                    old_metadata = old_lookup[path],
+                    new_metadata = curr_lookup[path]
+                )
+
+                modified_changes.append(new_file)
+            
+            elif curr_lookup[path].hash != old_lookup[path].hash:
+                new_file = Change(
+                    change_type = ChangeType.MODIFIED,
+                    old_metadata = old_lookup[path],
+                    new_metadata = curr_lookup[path]
+                )
+
+                modified_changes.append(new_file)
+
+    return modified_changes
+
+
+
+        
+
 
